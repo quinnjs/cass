46a6438410ed4367d503b204e8a2f717e94ea4b1
Jan Krems <jan.krems@groupon.com>
2016-09-05 16:17:19 -0700

    test: Cleanup of test cases

--- test/uri-template-trie.test.js
+++ test/uri-template-trie.test.js
@@ -1,6 +1,6 @@
 'use strict';
 const querystring = require('querystring');
-// const assert = require('assertive');
+const assert = require('assertive');
 
 const parseURITemplate = require('../lib/parse-uri-template');
 
@@ -192,35 +192,51 @@ class URITemplateTrie {
   }
 }
 
-describe('uri template tree', () => {
-  it('supports all kinds of stuff', () => {
+describe('uri template trie', () => {
+  describe('a trie', () => {
     const trie = new URITemplateTrie();
-    trie.insert('/', 'root-value');
-    trie.insert('/static', 'static-value');
-    trie.insert('{/id}', 'id-value');
-    trie.insert('/users{/id}', 'just-user-id-value');
-    trie.insert('/users{/id}{?show,query*}', 'user-id-value');
-    trie.insert('/stuff{?show}', 'stuff-value');
-    // TODO: Treat this as /posts{/year}{/month}{/slug}
-    trie.insert('/posts{/year,month,slug}', 'blog-value');
-    trie.insert('/users{/id}{/more*}', 'splat-value');
-    trie.insert('/~{user}{/more*}', 'home-value');
 
-    [
-      '/',
-      '/static',
-      '/some-id',
-      '/users/robin',
-      '/users/robin?show=full&some=more&qs=stuff',
-      '/stuff?show=id,name',
-      '/posts/2016/07/my%20post',
-      '/users/robin/one',
-      '/users/robin/one/and/two/and/three',
-      '/totally/a/404',
-      '/~robin/projects/quinn',
-    ].forEach(uri => {
-      const result = trie.find(uri);
-      console.log(uri, result);
+    before(() => {
+      trie.insert('/', 'root-value');
+      trie.insert('/static', 'static-value');
+      trie.insert('{/id}', 'id-value');
+      trie.insert('/users{/id}', 'just-user-id-value');
+      trie.insert('/users{/id}{?show,query*}', 'user-id-value');
+      trie.insert('/stuff{?show}', 'stuff-value');
+      trie.insert('/posts{/year,month,slug}', 'blog-value');
+      trie.insert('/users{/id}{/more*}', 'splat-value');
+      trie.insert('/~{user}{/more*}', 'home-value');
+    });
+
+    new Map([
+      ['/', { value: 'root-value', params: {} }],
+      ['/static', { value: 'static-value', params: {} }],
+      ['/some-id', { value: 'id-value', params: { id: 'some-id' } }],
+      ['/users/robin', { value: 'just-user-id-value', params: { id: 'robin' } }],
+      ['/users/robin?show=full&some=more&extra=stuff', {
+        value: 'user-id-value',
+        params: { id: 'robin', show: 'full', query: { some: 'more', extra: 'stuff' } },
+      }],
+      ['/stuff?show=id,name', { value: 'stuff-value', params: { show: 'id,name' } }],
+      ['/posts/2016/07/my%20post', {
+        value: 'blog-value',
+        params: { year: '2016', month: '07', slug: 'my post' },
+      }],
+      ['/users/robin/one', { value: 'splat-value', params: { id: 'robin', more: ['one'] } }],
+      ['/users/robin/one/and/two/and/three?is=ignored', {
+        value: 'splat-value',
+        params: { id: 'robin', more: ['one', 'and', 'two', 'and', 'three'] },
+      }],
+      ['/totally/a/404', null],
+      ['/~robin/projects/quinn', {
+        value: 'home-value',
+        params: { user: 'robin', more: ['projects', 'quinn'] },
+      }],
+    ]).forEach((expected, uri) => {
+      it(`routes ${uri}`, () => {
+        const result = trie.find(uri);
+        assert.deepEqual(expected, result);
+      });
     });
   });
 });
